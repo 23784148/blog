a32a9b995d51bbd1ccec3e2f5d7170496b02128a
Kenshin <fxblog@gmail.com>
2023-05-08 13:41:24 +0800

    Update issues to rss workflow.

--- bin/issues2rss.js
+++ bin/issues2rss.js
@@ -63,6 +63,8 @@ const fs          = require( 'fs' ),
     });
 
     issues.forEach( issue => {
+        issue = issue.node;
+        console.log( issue )
         feed.item({
             title: issue.title,
             description: issue.body,
